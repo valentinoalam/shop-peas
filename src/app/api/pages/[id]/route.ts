c07f8aa74be092463c58ed2d959d3b312e091082
Ichikyube <ichikyube@gmail.com>
2025-04-30 01:18:18 +0700

    uwaa

--- src/app/api/pages/[id]/route.ts
+++ src/app/api/pages/[id]/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
   try {
-    const id = params.id
+    const {id} = await params
 
     const page = await prisma.page.findUnique({
       where: { id },
