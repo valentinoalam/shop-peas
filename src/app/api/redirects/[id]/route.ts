fbd815f3a2e27972b0e28dca6a696ab8980bd8c7
Ichikyube <ichikyube@gmail.com>
2025-04-30 01:26:51 +0700

    uwaa

--- src/app/api/redirects/[id]/route.ts
+++ src/app/api/redirects/[id]/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
   try {
-    const id = params.id
+    const {id} = await params
 
     await prisma.redirect.delete({
       where: { id },
